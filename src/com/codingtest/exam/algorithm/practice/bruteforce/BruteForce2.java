f3d536e4a2f192023e3b7496cb69e1f932ad036d
simhw <simhw96@gmail.com>
2022-02-21 00:23:54 +0900

    완전 탐색(중복 허용 X, 순차 정렬) 중복 체크 리스트 추가

--- src/com/codingtest/exam/algorithm/practice/bruteforce/BruteForce2.java
+++ src/com/codingtest/exam/algorithm/practice/bruteforce/BruteForce2.java
@@ -16,7 +16,7 @@ public class BruteForce2 {
     }
 
     static int N, M;
-    static int[] selected;
+    static int[] selected, used;
 
     // M 개를 전부 고른 경우 조건에 맞는 탐색을 한 가지 성공한 것이고,
     // 아직 M 개를 고르지 않은 경우 k 번째부터 M 번째 원소를 조건에 맞게 고르는 모든 방법을 시도한다.
@@ -30,20 +30,31 @@ public class BruteForce2 {
         }
         else {
             for (int candidate = 1; candidate <= N; candidate++) {
-                // 중복을 체크해주는 알고리즘이 필요하다.
-                boolean isUsed = false;
-                for (int i = 0; i < k; i++) {
-                    if (candidate == selected[i]) {
-                        isUsed = true;
-                    }
-                }
-                // k 번째에 candidate 가 올 수 있다면 재귀함수를 호출해준다.
-                if (!isUsed) {
-                    selected[k] = candidate;
-                    // k + 1 번부터 M 번까지 채워주는 함수를 호출해준다.
-                    recFunc(k + 1);
-                    selected[k] = 0;
-                }
+
+                // (1) 중복을 체크해주는 알고리즘
+//                boolean isUsed = false;
+//                for (int i = 0; i < k; i++) {
+//                    if (candidate == selected[i]) {
+//                        isUsed = true;
+//                    }
+//                }
+
+                // (2) 중복을 체크해주는 알고리즘
+                if (used[candidate] == 1) continue;
+
+//                if (!isUsed) {
+//                    selected[k] = candidate;
+//                    // k + 1 번부터 M 번까지 채워주는 함수를 호출해준다.
+//                    recFunc(k + 1);
+//                    selected[k] = 0;
+//                }
+
+                selected[k] = candidate;
+                used[candidate] = 1;
+
+                recFunc(k + 1);
+                selected[k] = 0;
+                used[candidate] = 0;
             }
         }
     }
