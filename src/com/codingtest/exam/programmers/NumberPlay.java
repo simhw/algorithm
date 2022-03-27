0e2cfc628dff39110f24d2e61b5679cef21ca80b
simhw <simhw96@gmail.com>
2022-03-28 01:44:33 +0900

    프로그래머스 숫자 문자열과 영단어 (카카오 인턴십)

---
+++ src/com/codingtest/exam/programmers/NumberPlay.java
@@ -0,0 +1,34 @@
+package com.codingtest.exam.programmers;
+
+import java.util.HashMap;
+
+public class NumberPlay {
+    public int solution(String s) {
+        int answer = 0;
+
+        HashMap<String, Integer> words = new HashMap<>();
+        words.put("zero", 0);
+        words.put("one", 1);
+        words.put("two", 2);
+        words.put("three", 3);
+        words.put("four", 4);
+        words.put("five", 5);
+        words.put("six", 6);
+        words.put("seven", 7);
+        words.put("eight", 8);
+        words.put("nine", 9);
+
+        for (String word:words.keySet()) {
+            s = s.replace(word, words.get(word).toString());
+        }
+
+        answer = Integer.parseInt(s);
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        NumberPlay numberPlay = new NumberPlay();
+        String s = "one4seveneight";
+        numberPlay.solution(s);
+    }
+}
