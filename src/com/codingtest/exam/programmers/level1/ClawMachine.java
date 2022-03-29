d1fb061898351f0aea10e00c01ed2136ea164083
simhw <simhw96@gmail.com>
2022-03-29 22:48:05 +0900

    프로그래머스 크레인 인형뽑기 게임 (카카오 인턴) 런타임 에러 처릴

--- src/com/codingtest/exam/programmers/level1/ClawMachine.java
+++ src/com/codingtest/exam/programmers/level1/ClawMachine.java
@@ -10,38 +10,51 @@ public class ClawMachine {
 
         int answer = 0;
         ArrayList<Integer> basket = new ArrayList<>();
-        int move, idx; 
+        int move, idx;
 
-        for (int i = 0; i < moves.length; i++) {    
+        for (int i = 0; i < moves.length; i++) {
             move = moves[i] - 1;
             idx = 0;
-            
-            while(board[idx][move] == 0 && idx < board.length - 1) idx++;
+
+            while (board[idx][move] == 0 && idx < board.length - 1) idx++;
 
             int popped = 0;
-            if (basket.size() > 0) popped = basket.get(basket.size() - 1);
-            
-            if (popped == board[idx][move]) {
+            if (basket.size() > 0) {
+                popped = basket.get(basket.size() - 1);
+            }
+            /**
+             빈 board 을 peek 하는 경우 런타임 에러 발생
+             인형이 없는 곳에서 크레인을 작동시키는 경우에는 아무런 일도 일어나지 않습니다.
+             */
+            if (board[idx][move] == 0) {
+                continue;
+            }
+            // peek
+            else if (popped == board[idx][move]) {
                 answer += 2;
                 basket.remove(basket.size() - 1);
-                board[idx][move] = 0;   
-            } else {
+                board[idx][move] = 0;
+            }
+            // add
+            else {
                 basket.add(board[idx][move]);
-                board[idx][move] = 0;   
-            }    
+                board[idx][move] = 0;
+            }
         }
+        System.out.println(answer);
         return answer;
     }
+
     public static void main(String[] args) {
         ClawMachine clawMachine = new ClawMachine();
         int[][] board = new int[][]{
-            {0,0,0,0,0},
-            {0,0,1,0,3},
-            {0,2,5,0,1},
-            {4,2,4,4,2},
-            {3,5,1,3,1}
+                {0, 0, 1, 0, 0},
+                {0, 0, 1, 0, 0},
+                {0, 2, 1, 0, 0},
+                {0, 2, 1, 0, 0},
+                {0, 2, 1, 0, 0}
         };
-        int[] moves = new int[]{1,5,3,5,1,2,1,4};
+        int[] moves = new int[]{1, 5, 3, 5, 1, 2, 1, 4};
         clawMachine.solution(board, moves);
     }
 }
