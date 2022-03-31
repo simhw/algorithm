d1451a5d85605c8d1f5352795d5e901e7e867a0f
simhw <simhw96@gmail.com>
2022-03-31 17:19:58 +0900

    프로그래머스 실패율 (카카오 블라인드) 마지막 스테이지에 아무도 도달하지 못한 경우 예외 처리

--- src/com/codingtest/exam/programmers/level1/FailureRate.java
+++ src/com/codingtest/exam/programmers/level1/FailureRate.java
@@ -10,79 +10,80 @@ public class FailureRate {
         int[] answer = new int[N];
 
         // 0. 초기화
-        // 스테이지별 클리어하지 못한 플레이어 수 초기화
+        // 스테이지에 도달했으나 아직 클리어하지 못한 플레이어의 수
 
-        int stage;
+        int[] notClearPlayer = new int[N + 2];
+
         Stage[] stageList = new Stage[N + 1];
+
         for (int i = 0; i < stages.length; i++) {
-            stage = stages[i];
-            if (stage > N)
-                continue;
-            else {
-                if (stageList[stage] == null) {
-                    stageList[stage] = new Stage(stage);
-                    stageList[stage].reachedPlayer = 1;
-                } else {
-                    stageList[stage].reachedPlayer += 1;
-                }
-            }
+            notClearPlayer[stages[i]] += 1;
         }
 
         // 1. 실패율 계산
-        // 스테이지에 도달했으나 아직 클리어하지 못한 플레이어의 수 / 스테이지에 도달한 플레이어 수
+        // 실패율 = 스테이지에 도달했으나 아직 클리어하지 못한 플레이어의 수 / 스테이지에 도달한 플레이어 수
 
         int reachedPlayer = stages.length;
+        stageList[0] = new Stage(0, -1.0);
 
-        for (int i = 0; i < stageList.length; i++) {
+        for (int i = 1; i < stageList.length; i++) {
+
+            System.out.println(notClearPlayer[i] + "/" + reachedPlayer);
+
+            // ※ 예외 처리 
+            // 마지막 스테이지에 아무도 도달하지 못한 경우 마지막 스테이지의 실패율은 0이다. 
+            // 0 / 0
             
-            if (stageList[i] == null) {
-                if (i == 0) {
-                    stageList[i] = new Stage(i);
-                    stageList[i].failRate = -1;
-                } else stageList[i] = new Stage(i);
+            if (reachedPlayer == 0) {
+                stageList[i] = new Stage(i, 0.0);
             } else {
-                // 이전 스테이지를 통과한 플레이어들을 빼준다.
-                reachedPlayer -= stageList[i - 1].reachedPlayer;
-                stageList[i].failRate = (stageList[i].reachedPlayer * 1.0) / reachedPlayer;
+                stageList[i] = new Stage(i, (double) notClearPlayer[i] / reachedPlayer);
             }
+            reachedPlayer -= notClearPlayer[i];
         }
 
         // 2. 정렬
         Arrays.sort(stageList, new Comparator<Stage>() {
             @Override
             public int compare(Stage o1, Stage o2) {
-                if (o1.failRate < o2.failRate) return 1;
-                else if (o1.failRate == o2.failRate) {
-                    if (o1.stage > o2.stage) return 1;
-                    else return -1;  
-                } 
-                else return -1;
+                if (o1.failRate < o2.failRate) {
+                    return 1;
+                } else if (o1.failRate == o2.failRate) {
+                    if (o1.stage > o2.stage) {
+                        return 1;
+                    } else {
+                        return -1;
+                    }
+                } else {
+                    return -1;
+                }
             }
         });
 
         for (int i = 0; i < answer.length; i++) {
             answer[i] = stageList[i].stage;
+            System.out.print(answer[i] + " ");
         }
         return answer;
     }
 
     public static void main(String[] args) {
         FailureRate failureRate = new FailureRate();
-        failureRate.solution(5, new int[] { 2, 1, 2, 6, 2, 4, 3, 3 });
+        failureRate.solution(8, new int[] { 1, 2, 3, 3, 4, 6, 7 });
     }
 }
 
 class Stage {
     int stage;
-    int reachedPlayer;
     double failRate;
 
-    Stage(int stage) {
+    Stage(int stage, double failRate) {
         this.stage = stage;
+        this.failRate = failRate;
     }
 
     @Override
     public String toString() {
-        return "stage: " + stage + ", reachedPlayer: " + reachedPlayer + ", failRate: " + failRate;
+        return "stage: " + stage + ", failRate: " + failRate;
     }
 }
