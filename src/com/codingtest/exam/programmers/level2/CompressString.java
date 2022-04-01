9d5586828102e4e4f57ca10faff320d93f3a3c59
simhw <simhw96@gmail.com>
2022-04-01 16:40:20 +0900

    프로그래머스 문자열 압축(카카오 블라인드)

--- src/com/codingtest/exam/programmers/level2/CompressString.java
+++ src/com/codingtest/exam/programmers/level2/CompressString.java
@@ -7,56 +7,63 @@ public class CompressString {
         // 문자열을 자르는 단위 k
         // 압축할 문자열 s가 매개변수로 주어질 때, 1개 이상 단위로 문자열을 잘라 압축한다.
 
+        /**
+         * 테스트 케이스 5
+         * 입력 문자열 길이가 1일 경우 
+         */
+        
+        if (s.length() < 2) 
+            answer = 1;
+        
         for (int k = 1; k < s.length() / 2 + 1; k++) {
 
             int len = 0;
-            int cnt = 0;
+            int cnt = 1;
 
             for (int i = 0; i < s.length(); i += k) {
 
-                if (i + k + k > s.length())
+                if (i + k + k > s.length()){
                     break;
+                }
 
                 // 문자열은 제일 앞부터 정해진 길이만큼 잘라야 합니다.
-                String str1 = s.substring(i, i + k);
-                String str2 = s.substring(i + k, i + k + k);
+                System.out.println(s.substring(i, i + k) + " " + s.substring(i + k, i + k + k));
 
-                System.out.println(str1 + " " + str2);
-
-                if (str1.equals(str2)) {
+                if (s.substring(i, i + k).equals(s.substring(i + k, i + k + k))) {
                     cnt++;
                 } else {
-                    if (cnt > 0) {
+                    if (cnt > 1) {
+                        // 중복하는 문자열 길이가 두자리수일 경우를 고려해 숫자의 길이를 더해준다.
                         len += Integer.toString(cnt).length();
                         len += k;
                         System.out.println("len " + len);
-                        cnt = 0;
+                        cnt = 1;
                     } else {
                         len += k;
                         System.out.println("len " + len);
-
                     }
                 }
             }
 
-            if (cnt > 0) {
+            if (cnt > 1) {
                 len += Integer.toString(cnt).length();
                 len += k;
+                // 탐색하지 않은 나머지 배열 
+                len += s.length() % k;
+
             } else {
                 len += k;
                 // 탐색하지 않은 나머지 배열 
                 len += s.length() % k;
             }
-            System.out.println("len " + len);
+            System.out.println("total " + len);
             answer = answer < len ? answer : len;
-
         }
-        System.out.println(answer);
         return answer;
     }
 
     public static void main(String[] args) {
         CompressString compressString = new CompressString();
-        compressString.solution("aabbaccc");
+        compressString.solution("a");
     }
 }
