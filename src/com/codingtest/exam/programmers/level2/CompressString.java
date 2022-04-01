e102ffe1c2be353a4d7ee3ddf3cb552c52f039b4
simhw <simhw96@gmail.com>
2022-04-01 16:08:50 +0900

    프로그래머스 문자열 압축 (카카오 블라인드) 테스트케이스 2., 3, 5, 6, 11, 12, 14, 15, 17, 20 실패

--- src/com/codingtest/exam/programmers/level2/CompressString.java
+++ src/com/codingtest/exam/programmers/level2/CompressString.java
@@ -1,35 +1,58 @@
 package com.codingtest.exam.programmers.level2;
-
 public class CompressString {
 
     public int solution(String s) {
-        int answer = 0;
-        compress(1, s);
-        return answer;
-    }
+        int answer = 1001;
 
-    public int compress(int k, String s) {
-        int len = s.length();
-        int cnt = 0;
+        // 문자열을 자르는 단위 k
+        // 압축할 문자열 s가 매개변수로 주어질 때, 1개 이상 단위로 문자열을 잘라 압축한다.
 
-        for (int i = 0; i < s.length() - 1; i++) {
+        for (int k = 1; k < s.length() / 2 + 1; k++) {
 
-            String str = s.substring(i, i + k);
-            String next = s.substring(i + k, (i + k) + k);
+            int len = 0;
+            int cnt = 0;
 
-            System.out.println(str + " " + next);
-            if (str.equals(next)) {
-                cnt++;
-            } else {
-                // 축약된 길이 만큼 빼준다.
-                System.out.println(cnt);
-                len = len - (cnt * k) + 1;
-                cnt = 0;
+            for (int i = 0; i < s.length(); i += k) {
+
+                if (i + k + k > s.length())
+                    break;
+
+                // 문자열은 제일 앞부터 정해진 길이만큼 잘라야 합니다.
+                String str1 = s.substring(i, i + k);
+                String str2 = s.substring(i + k, i + k + k);
+
+                System.out.println(str1 + " " + str2);
+
+                if (str1.equals(str2)) {
+                    cnt++;
+                } else {
+                    if (cnt > 0) {
+                        len += Integer.toString(cnt).length();
+                        len += k;
+                        System.out.println("len " + len);
+                        cnt = 0;
+                    } else {
+                        len += k;
+                        System.out.println("len " + len);
+
+                    }
+                }
             }
+
+            if (cnt > 0) {
+                len += Integer.toString(cnt).length();
+                len += k;
+            } else {
+                len += k;
+                // 탐색하지 않은 나머지 배열 
+                len += s.length() % k;
+            }
+            System.out.println("len " + len);
+            answer = answer < len ? answer : len;
+
         }
-        // 문자열 길이 반환
-        System.out.println("len " + len);
-        return len;
+        System.out.println(answer);
+        return answer;
     }
 
     public static void main(String[] args) {
