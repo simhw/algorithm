290953cdce4cdde1c5e5b8813102d9c749d0d400
simhw <simhw96@gmail.com>
2022-04-04 13:20:54 +0900

    프로그래머스 오픈채팅방(카카오 블라인드)

--- src/com/codingtest/exam/programmers/level2/OpenChatting.java
+++ src/com/codingtest/exam/programmers/level2/OpenChatting.java
@@ -3,43 +3,36 @@ package com.codingtest.exam.programmers.level2;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-/**
- * OpenChatting
- */
-
-
 public class OpenChatting {
     public String[] solution(String[] record) {
         HashMap<String, String > members = new HashMap<>();
         ArrayList<String> output = new ArrayList<>();
-        String[] str = new String[3];
+                
+        String[] splited = new String[3];
+
         // 0. 사람 정보 초기화
         for (int i = 0; i < record.length; i++) {
-            str = record[i].split(" ");
-            String event = str[0];
-            switch (event) {
-                case "Enter":
-                case "Change":
-                    members.put(str[1], str[2]);
-                    break;
-                case "Leave":
-                    members.remove(str[1]);
-                    break;
-                default:
-                    break;
+            splited = record[i].split(" ");
+            if (splited[0].equals("Enter")) {
+                members.put(splited[1], splited[2]);
+            } else if (splited[0].equals("Change")) {
+                members.put(splited[1], splited[2]);
+            } else {
+                // members.remove(splited[1]);
+            }
+        }        
+        // 1. 출력
+        for (int i = 0; i < record.length; i++) {
+            splited = record[i].split(" ");
+            if (splited[0].equals("Enter")) {
+                output.add(members.get(splited[1]) + "님이 들어왔습니다.");
+            } else if (splited[0].equals("Leave")) {
+                output.add(members.get(splited[1]) + "님이 나갔습니다.");
             }
         }
-        // 1. 출력
-        String nickName;
-        for (int i = 0; i < record.length; i++) {
-            str = record[i].split(" ");
-            nickName = members.get(str[1]);
-            if (str[0].equals("Enter")) {
-                output.add(nickName + "님이 들어왔습니다.");
-            } else if (str[0].equals("Leave")) {
-                output.add(nickName + "님이 나갔습니다.");
-            } else continue;
-        }
+
+        System.out.println(output.toString());
+        System.out.println(members.toString());
 
         String[] answer = new String[output.size()];
         return output.toArray(answer);
