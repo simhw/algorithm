e5dcb63a33a5d4b1e50b9ce64dccd872098987da
simhw <simhw96@gmail.com>
2022-04-05 19:39:02 +0900

    프로그래머스 메뉴 리뉴얼(카카오 블라인드)

--- src/com/codingtest/exam/programmers/level2/RenewalMenu.java
+++ src/com/codingtest/exam/programmers/level2/RenewalMenu.java
@@ -1,39 +1,58 @@
 package com.codingtest.exam.programmers.level2;
 
-import java.util.HashMap;
+import java.util.*;
 
 public class RenewalMenu {
-    static HashMap<String, Integer> hashMap = new HashMap<>();
-    static String[] selected;
-    static String[] order;
-    static int M;
+    private static HashMap<String, Integer> hashMap = new HashMap<>();
+    private static String[] selected;
+    private static List<String> order = new ArrayList<>();
+    private static int M;
 
     public String[] solution(String[] orders, int[] course) {
-        String[] answer = {};
-        for (int i = 0; i < orders.length; i++) {
-            for (int j = 0; j < course.length; j++) {
-                selected = new String[course[j] + 1];
-                order = orders[i].toUpperCase().split("");
-                M = course[j];
+        ArrayList<String> arrayList = new ArrayList<>();
+
+        for (int i = 0; i < course.length; i++) {
+            for (int j = 0; j < orders.length; j++) {
+                order = Arrays.asList(orders[j].split(""));
+                Collections.sort(order);
+                M = course[i];
+                selected = new String[M + 1];
                 recFunc(1);
-                System.out.println(hashMap.toString());
             }
+            if (hashMap.size() == 0) continue;                
+            int max = Collections.max(hashMap.values());
+            for (String key : hashMap.keySet()) {
+                if (max > 1 && max == hashMap.get(key)) {
+                    arrayList.add(key);
+                }
+            } 
+            hashMap = new HashMap<>();
         }
-        return answer;
+        arrayList.sort(Comparator.naturalOrder());
+
+        String[] answer = new String[arrayList.size()];
+        arrayList.toArray(answer);
+      
+        return answer;        
     }
+
     public void recFunc(int k) {
-        // 코스요리 메뉴는 최소 2가지 이상의 단품메뉴로 구성하려고 합니다. 
-        // 또한, 최소 2명 이상의 손님으로부터 주문된 단품메뉴 조합에 대해서만 코스요리 메뉴 후보에 포함하기로 했습니다.
+        // M 개를 전부 고른 경우 조건에 맞는 탐색을 한 가지 성공한 것이다.
         if (k == M + 1) {
-            String str = null;
+            String str = "";
             for (int i = 1; i < M + 1; i++) {
-                 str.concat(selected[i]);
+                str += selected[i];
             }
-            hashMap.put(str, 0);
-        }
-        else {
-            for (int i = 0; i < order.length; i++) {
-                selected[k] = order[i];
+            if (hashMap.get(str) == null) {
+                hashMap.put(str, 1);
+            } else {
+                hashMap.put(str, hashMap.get(str) + 1);
+            }
+        } else {
+            // 아직 M 개를 고르지 않은 경우 k 번째부터 M 번째 원소를 조건에 맞게 고르는 모든 방법을 시도한다.
+            int start = order.indexOf(selected[k - 1]) + 1;
+            for (int i = start; i < order.size(); i++) {
+                selected[k] = order.get(i);
                 recFunc(k + 1);
                 selected[k] = null;
             }
@@ -42,6 +61,6 @@ public class RenewalMenu {
 
     public static void main(String[] args) {
         RenewalMenu renewalMenu = new RenewalMenu();
-        renewalMenu.solution(new String[]{"XYZ"}, new int[]{2});
+        renewalMenu.solution(new String[] { "ABCDE", "AB", "CD", "ADE", "XYZ", "XYZ", "ACD" }, new int[] { 2, 3, 5});
     }
 }
