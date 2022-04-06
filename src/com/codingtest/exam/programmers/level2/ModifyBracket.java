94c08d5e8bd2326d3be360444b1f08db3df3ee34
simhw <simhw96@gmail.com>
2022-04-07 01:53:16 +0900

    프로그래머스 괄호 변환(카카오 블라인드) 리스트에서 문자열로 변경

--- src/com/codingtest/exam/programmers/level2/ModifyBracket.java
+++ src/com/codingtest/exam/programmers/level2/ModifyBracket.java
@@ -1,9 +1,5 @@
 package com.codingtest.exam.programmers.level2;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 import java.util.Stack;
 
 public class ModifyBracket {
