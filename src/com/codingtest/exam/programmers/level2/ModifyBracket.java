e86657631c026896e08e73282dedb6276f3266be
simhw <simhw96@gmail.com>
2022-04-06 19:10:38 +0900

    프로그래머스 괄호 변환(카카오 블라인드) 4단계 구현 OutOfMemoryError 발생

--- src/com/codingtest/exam/programmers/level2/ModifyBracket.java
+++ src/com/codingtest/exam/programmers/level2/ModifyBracket.java
@@ -21,6 +21,7 @@ public class ModifyBracket {
     public List<String> separate(List<String> w) {
         List<String> u, v;
         Stack<String> stack = new Stack<>();
+        List<String> added = new ArrayList<String>();
 
         // 1. 입력이 빈 문자열인 경우, 빈 문자열을 반환합니다.
         if (w.size() == 0) {
@@ -48,24 +49,28 @@ public class ModifyBracket {
             if (u.get(0).equals("(")) {  
                 // 3. 문자열 u가 "올바른 괄호 문자열" 이라면 문자열 v에 대해 1단계부터 다시 수행합니다.   
                 // 3-1. 수행한 결과 문자열을 u에 이어 붙인 후 반환합니다. 
-                Collections.addAll(u, separate(v).toArray(new String[0]));
-                return u;
+                added.addAll(u);
+                added.addAll(separate(v));
             } else {
                 // 4. 문자열 u가 "올바른 괄호 문자열"이 아니라면 아래 과정을 수행합니다. 
-                // 4-1. 빈 문자열에 첫 번째 문자로 '('를 붙입니다. 
-                
+                // 4-1. 빈 문자열에 첫 번째 문자로 '('를 붙입니다.
+                List<String> list = new ArrayList<String>();
+
                 // 4-2. 문자열 v에 대해 1단계부터 재귀적으로 수행한 결과 문자열을 이어 붙입니다. 
                 // 4-3. ')'를 다시 붙입니다. 
                 // 4-4. u의 첫 번째와 마지막 문자를 제거하고, 나머지 문자열의 괄호 방향을 뒤집어서 뒤에 붙입니다. 
-                // 4-5. 생성된 문자열을 반환합니다.
-                return u;
+                added.addAll(u);
+                added.addAll(v);
 
+                // 4-5. 생성된 문자열을 반환합니다.
             }
+            System.out.println("addAll " + added);
+            return added;
         }
     }
 
     public static void main(String[] args) {
         ModifyBracket modifyBracket = new ModifyBracket();
-        modifyBracket.solution("(()())()");
+        modifyBracket.solution("()))((()");
     }
 }
