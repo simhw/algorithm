86dbc4f9b417b1ddae588c14fbfd4110fe0aaa94
simhw <simhw96@gmail.com>
2022-04-07 18:54:29 +0900

    프로그래머스 뉴스 클러스터링(카카오 블라인드) 테스트케이스 4, 7, 9, 10, 11 실패

--- src/com/codingtest/exam/programmers/level2/NewsClustering.java
+++ src/com/codingtest/exam/programmers/level2/NewsClustering.java
@@ -1,12 +1,9 @@
 package com.codingtest.exam.programmers.level2;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Locale;
 
 public class NewsClustering {
     public int solution(String str1, String str2) {
-        int answer = 0;
         ArrayList<String> list1 = new ArrayList<>();
         ArrayList<String> list2 = new ArrayList<>();
 
@@ -18,18 +15,47 @@ public class NewsClustering {
 
         // 1. 입력으로 들어온 문자열은 두 글자씩 끊어서 다중집합의 원소로 만든다.
         for (int i = 0; i < str1.length() - 1; i++) {
-            list1.add(str1.substring(i, i + 2));
+            if (str1.substring(i, i + 2).matches("^[A-Z]*$")) {
+                list1.add(str1.substring(i, i + 2));
+            }
         }
         for (int i = 0; i < str2.length() - 1; i++) {
-            list2.add(str2.substring(i, i + 2));
+            if (str2.substring(i, i + 2).matches("^[A-Z]*$")){            
+                list2.add(str2.substring(i, i + 2));
+            }
         }
 
         System.out.println(list1);
         System.out.println(list2);
-        return answer;
+
+        // 2. 자카드 유사도 
+        // 두 집합의 교집합 크기를 두 집합의 합집합 크기로 나눈 값으로 정의된다.
+
+        if (list1.size() == 0 && list2.size() == 0) {
+            return 65536;
+        } else {
+            int intersection = 0, union = 0;
+            for (String str : list1) {
+                if (list2.indexOf(str) != -1) {
+                    intersection++;
+                }
+            }
+            
+            list1.addAll(list2);
+            union = list1.size() - intersection;
+            double similarity = (double)intersection / (double)union;
+            return  (int)similarity * 65536;
+        }
     }
+
     public static void main(String[] args) {
         NewsClustering newsClustering = new NewsClustering();
-        newsClustering.solution("FRANCE", "french");
+        newsClustering.solution("E=M*C^2", "e=m*c^2");
     }
 }
+
+// 테스트 4 〉	실패 (8.29ms, 75.9MB)
+// 테스트 7 〉	실패 (1.06ms, 72.5MB)
+// 테스트 9 〉	실패 (0.90ms, 73.4MB)
+// 테스트 10 〉	실패 (1.67ms, 74.6MB)
+// 테스트 11 〉	실패 (3.37ms, 77.7MB)
