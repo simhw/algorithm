7b5a4465cbc84427b2070c4edf7244e317c41af3
simhw <simhw96@gmail.com>
2022-05-04 18:14:11 +0900

    에라토스테네스 체

---
+++ src/com/codingtest/exam/programmers/level2/SearchPrimeNumber.java
@@ -0,0 +1,48 @@
+package com.codingtest.exam.programmers.level2;
+
+import java.util.Arrays;
+
+public class SearchPrimeNumber {
+    public int solution(int n, int k) {
+        int answer = -1;
+        return answer;
+    }
+
+    public boolean isPrimeNumber(int x) {
+        int end = (int) Math.sqrt(x);
+        System.out.println("end = " + end);
+        for (int i = 2; i <= end; i++) {
+            if (x % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+ 
+    // 에라토스테네스 체 
+    // 대량의 소수를 한번에 판별하고자 할 때 사용한다. 
+    public void primNumberSieve(int number) {
+        int[] a = new int[number + 1];
+
+        // 1. 소수를 판별할 범위만큼 배열을 할당하고 값을 초기화한다. 
+        for (int i = 2; i <= number; i++) {
+            a[i] = i;
+        }
+
+        // 2. 2부터 시작해서 특정 숫자의 배수에 해당하는 숫자들을 모두 지운다.
+        for (int i = 2; i <= number; i++) {
+            // 3. 이미 지워진 숫자의 경우 건너뛴다.  
+            if (a[i] == 0) continue;
+            for (int j = i + i; j <= number; j += i) {
+                a[j] = 0;
+            }
+        }
+        System.out.println(Arrays.toString(a));
+    }
+
+    public static void main(String[] args) {
+        SearchPrimeNumber searchPrimeNumber = new SearchPrimeNumber();
+        // searchPrimeNumber.solution(437674, 3);
+        searchPrimeNumber.primNumberSieve(25);
+    }
+}
