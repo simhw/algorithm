c94dad79ecc3d2c2ef6db1ccad0acd5b62e6ebd9
simhw <simhw96@gmail.com>
2022-05-20 18:49:50 +0900

    프로그래머스 양궁대회 정확성 42.9

--- src/com/codingtest/exam/programmers/level2/ArcheryCompetition.java
+++ src/com/codingtest/exam/programmers/level2/ArcheryCompetition.java
@@ -1,25 +1,71 @@
 package com.codingtest.exam.programmers.level2;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+
 public class ArcheryCompetition {
+
+    static HashMap<Integer, List<Integer>> hashMap = new HashMap<>();
+
     public int[] solution(int n, int[] info) {
-        int[] answer = {};
-        int[] selected = new int[info.length];
-        // 11 개 과녁 점수 중 어피치보다 1발을 더 맞추거나 0발 맞추거나  
-        // 2의 11승 = 1,024 가지 
-        
-        int binary = 1;
-        for (int i = 0; i < 11; i++) {
-            binary *= 2;
+        int[] answer;
+
+        // 각 점수를 아예 안 맞추거나 어피치보다 1발을 더 맞히는 경우로 각 점수(10점 ~ 0점)마다 2가지(총 2,048가지)를 완전 탐색한다.
+        recursion(10, new int[11], n, info);
+        System.out.println(hashMap);
+
+        if (hashMap.size() > 0) {
+            answer = new int[11];
+            int max = Collections.max(hashMap.keySet());
+            for (int i = 0; i < answer.length; i++) {
+                answer[i] = hashMap.get(max).get(i);
+            }
+        } else {
+            answer = new int[]{-1};
         }
-        System.out.println(binary);
         return answer;
     }
-    public void recursion() {
 
+    public void recursion(int k, int[] scores, int n, int[] info) {
+        if (k == -1) {
+            int apeach = 0; int lion = 0;
+            if (n < 0) {
+                return;
+            } else {
+                scores[10] = n;
+            }
+            
+            for (int i = 0; i < scores.length; i++) {
+                if (scores[i] > info[i]) {
+                    lion += (10 - i);
+                } else {
+                    if (info[i] != 0) apeach += (10 - i); 
+                }
+            }
+
+            if (lion > apeach) {
+                List<Integer> list = new ArrayList<>();
+                for (int i = 0; i < scores.length; i++) {
+                    list.add(scores[i]);
+                }
+                hashMap.put(lion, list);
+            }
+
+        } else {
+            // k 점을 맞추는 경우
+            scores[10 - k] = info[10 - k] + 1;
+            recursion(k - 1, scores, n - scores[10 - k], info);
+            // k 점을 안 맞추는 경우
+            scores[10 - k] = 0;
+            recursion(k - 1, scores, n + scores[10 - k], info);
+        }
     }
+
     public static void main(String[] args) {
         ArcheryCompetition archeryCompetition = new ArcheryCompetition();
-        archeryCompetition.solution(5, new int[]{2, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0});
+        archeryCompetition.solution(10, new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 3, 4, 3 }); // 1,1,1,1,1,1,1,1,0,0,2
     }
-    
+
 }
