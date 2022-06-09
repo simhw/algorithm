b02ae80f3182603005b92478f314cdc63361b2de
simhw <simhw96@gmail.com>
2022-06-09 22:55:04 +0900

    이것이 코딩 테스트다 chapter08 다이나믹 프로그래밍

--- src/com/codingtest/exam/thisiscodingtest/ch08/Ex05.java
+++ src/com/codingtest/exam/thisiscodingtest/ch08/Ex05.java
@@ -12,39 +12,51 @@ public class Ex05 {
     static int N, M;
     static int[] current;
     static int[] dp;
+
     static void input() {
-        Scanner sc = new Scanner(System.in); 
-        
+        Scanner sc = new Scanner(System.in);
+
         N = sc.nextInt();   // 화폐 단위 개수 
-        M = sc.nextInt();   // 금액 
+        M = sc.nextInt();   // 화폐 개수 최소한의 금액
         current = new int[N];
         for (int i = 0; i < N; i++) {
             current[i] = sc.nextInt();
         }
+        // 초기화
+        // 10001 은 특정 금액을 만들 수 있는 화폐 구성이 가능하지 않다는 의미
         dp = new int[M + 1];
+        // 0 원의 경우, 화폐를 하나도 사용하지 않았을 때 만들 수 있으므로 0 으로 설정
         for (int i = 1; i < M + 1; i++) {
             dp[i] = 10001;
         }
     }
+
     static void solution() {
         for (int i = 0; i < N; i++) {
             int x = current[i];
             dp[x] = 1;
         }
 
-        for (int i = current[N - 1] + 1; i <= M; i++) {
-            for (int j = 0; j < N; j++) {
-                int k = current[j];
-                if (i - k >= k) {
-                    dp[i] = Math.min(dp[i], dp[i - k] + 1);
+        for (int i = 0; i < N; i++) {
+            int k = current[i];
+            for (int j = k; j < M + 1; j++) {
+                // (i - k) 원을 만드는 방법이 존재하는 경우
+                if (dp[j - k] != 10001) {
+                    dp[j] = Math.min(dp[j], dp[j - k] + 1);
+                    System.out.println(Arrays.toString(dp));
                 }
             }
-        }   
-        System.out.println(Arrays.toString(dp));
+        }
+        if (dp[M] == 10001) {
+            System.out.println(-1);
+        } else {
+            System.out.println(dp[M]);
+        }
     }
 
     public static void main(String[] args) {
-
+        input();
+        solution();
 /*
  2 15
  2
@@ -55,7 +67,5 @@ public class Ex05 {
  5
  7
  */
-        input();
-        solution();
     }
 }
