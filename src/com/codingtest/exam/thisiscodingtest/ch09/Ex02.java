fbc4ba01a7edec5dae1d8b9570be4edeb63c3b2a
simhw <simhw96@gmail.com>
2022-06-11 00:11:18 +0900

    이것이 코딩 테스트다 chapter09 최단 경로 merge

--- src/com/codingtest/exam/thisiscodingtest/ch09/Ex02.java
+++ src/com/codingtest/exam/thisiscodingtest/ch09/Ex02.java
@@ -1,17 +1,5 @@
 package com.codingtest.exam.thisiscodingtest.ch09;
 
-<<<<<<< HEAD
-// 개선된 다익스트라 최단 경로 알고리즘
-
-import java.util.PriorityQueue;
-
-public class Ex02 {
-    public static void main(String[] args) {
-        PriorityQueue<Node> queue = new PriorityQueue<>();
-        // 현재 가장 가까운 노드를 저장하기 위한 목적으로만 우선순위 큐 이용
-    }
-}
-=======
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.PriorityQueue;
@@ -91,4 +79,3 @@ public class Ex02 {
         solution();
     }
 }
->>>>>>> origin/master
--- src/com/codingtest/exam/thisiscodingtest/ch09/Node.java
+++ src/com/codingtest/exam/thisiscodingtest/ch09/Node.java
@@ -1,29 +1,14 @@
 package com.codingtest.exam.thisiscodingtest.ch09;
 
-<<<<<<< HEAD
-public class Node {
-    int vertex; // 정점
-    int edge;   // 간선
-
-    Node(int vertex, int edge) {
-=======
 public class Node implements Comparable<Node>{
     int vertex;
     int edge;
     
     Node (int vertex, int edge) {
->>>>>>> origin/master
         this.vertex = vertex;
         this.edge = edge;
     }
 
-<<<<<<< HEAD
-    @Override
-    public String toString() {
-        return "{vertex=" + vertex + ", edge=" + edge + '}';
-    }
-}
-=======
     public String toString() {
         return "{vertex:" + this.vertex + ", edge:" + this.edge + "}";
     }
@@ -34,4 +19,3 @@ public class Node implements Comparable<Node>{
         return this.edge - o.edge;
     }
 }
->>>>>>> origin/master
