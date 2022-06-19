9f6d1848288617ad9c3a6a8b6e85c17994f7aeea
simhw <simhw96@gmail.com>
2022-06-20 02:14:15 +0900

    chapter12 구현 문제

---
+++ src/com/codingtest/exam/thisiscodingtest/ch12/Q07.java
@@ -0,0 +1,59 @@
+package com.codingtest.exam.thisiscodingtest.ch12;
+
+// 럭키 스트레이트
+
+import java.util.Scanner;
+
+public class Q07 {
+    static String str;
+    static char[] arr;
+
+    static void input() {
+        Scanner sc = new Scanner(System.in);
+        str = sc.nextLine();
+        System.out.println(str);
+        arr = str.toCharArray();
+    }
+
+    static void solution() {
+        int len = str.length();
+        int sum = 0;
+        // 왼쪽 부분 자릿수 합 더하기
+        for (int i = 0; i < len / 2; i++) {
+            sum += (int) arr[i];
+        }
+        // 오른쪽 부분 자릿수 합 빼기
+        for (int i = len / 2; i < len; i++) {
+            sum -= (int) arr[i];
+        }
+        if (sum == 0) {
+            System.out.println("LUCKY");
+        } else {
+            System.out.println("READY");
+        }
+
+    }
+
+    static void mysolution() {
+        int mid = arr.length / 2;
+        int left = 0;
+        int right = 0;
+        for (int i = 0; i < mid; i++) {
+            left += arr[i] - 48;
+        }
+        for (int i = mid; i < arr.length; i++) {
+            right += arr[i] - 48;
+        }
+
+        if (left == right) {
+            System.out.println("LUCKY");
+        } else {
+            System.out.println("READY");
+        }
+    }
+
+    public static void main(String[] args) {
+        input();
+        solution();
+    }
+}
---
+++ src/com/codingtest/exam/thisiscodingtest/ch12/Q08.java
@@ -0,0 +1,59 @@
+package com.codingtest.exam.thisiscodingtest.ch12;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+
+// 문자열 재정렬
+public class Q08 {
+    static String S;
+
+    static void input() {
+        Scanner sc = new Scanner(System.in);
+        S = sc.nextLine();
+    }
+
+    static void solution() {
+        ArrayList<Character> result = new ArrayList<>();
+        int value = 0;
+        for (int i = 0; i < S.length(); i++) {
+            // 함수를 사용하여 알파벳과 숫자를 구분
+            if (Character.isAlphabetic(S.charAt(i))) {
+                result.add(S.charAt(i));
+            } else {
+                value += S.charAt(i) - '0';
+            }
+        }
+        Collections.sort(result);
+
+        for (int i = 0; i < result.size(); i++) {
+            System.out.print(result.get(i));
+        }
+        if (value != 0) {
+            System.out.println(value);
+        }
+    }
+
+    static void mysolution() {
+        String str = "";
+        int num = 0;
+        for (int i = 0; i < S.length(); i++) {
+            if ((S.charAt(i) - 'A') >= 0 && (S.charAt(i) - 'A') <= 25) {
+                str += S.charAt(i);
+            } else {
+                num += S.charAt(i) - '0';
+            }
+        }
+        char[] arr = str.toCharArray();
+        Arrays.sort(arr);
+        String sorted = new String(arr);
+        System.out.println(sorted + num);
+    }
+
+    public static void main(String[] args) {
+        // K1KA5CB7
+        input();
+        solution();
+    }
+}
