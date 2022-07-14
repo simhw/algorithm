2a66f31b385c12130af82835bcb7072503208d16
simhw <simhw96@gmail.com>
2022-07-15 00:36:36 +0900

    chapter13 DFS/BFS 감시 피하기 상하좌우 감시 메서드 수정

--- src/com/codingtest/exam/thisiscodingtest/ch13/Q20.java
+++ src/com/codingtest/exam/thisiscodingtest/ch13/Q20.java
@@ -2,19 +2,16 @@ package com.codingtest.exam.thisiscodingtest.ch13;
 
 import java.util.Scanner;
 
-//
+// 감시 피하기
 public class Q20 {
-
     static int n;
-    static int[][] arr;
+    static int[][] board;
     static int[][] temp;
-    static int[] dx = {-1, 1, 0, 0};
-    static int[] dy = {0, 0, -1, 1};
-
     static boolean answer;
 
     static void solution() {
         temp = new int[n][n];
+        // 빈 공간에 장애물 3개를 설치하는 모든 경우
         combination(0);
 
         if (answer) {
@@ -28,14 +25,15 @@ public class Q20 {
         if (k == 3) {
             for (int i = 0; i < n; i++) {
                 for (int j = 0; j < n; j++) {
-                    temp[i][j] = arr[i][j];
+                    temp[i][j] = board[i][j];
                 }
             }
-
+            // 장애물 설치 이후, 한 명이라도 학생이 감지되는지 검사
             for (int i = 0; i < n; i++) {
                 for (int j = 0; j < n; j++) {
+                    // 모든 선생님의 위치를 하나씩 확인
                     if (temp[i][j] == 2) {
-                        if (!dfs(i, j)) {
+                        if (!search(i, j)) {
                             return;
                         }
                     }
@@ -46,28 +44,79 @@ public class Q20 {
         }
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                if (arr[i][j] == 0) {
-                    arr[i][j] = 3;
+                if (board[i][j] == 0) {
+                    board[i][j] = 3;
                     combination(k + 1);
-                    arr[i][j] = 0;
+                    board[i][j] = 0;
                 }
             }
         }
     }
 
-    static boolean dfs(int x, int y) {
+    // 상, 하, 좌, 우 방향으로 감시를 진행
+    static boolean search(int x, int y) {
+        int nx = 0;
+        int ny = 0;
         for (int i = 0; i < 4; i++) {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-            if (nx < 0 || nx >= n || ny < 0 || ny >= n) {
-                continue;
+            // 위쪽 방향으로 감시
+            if (i == 0) {
+                nx = x + (-1);
+                while (nx >= 0) {
+                    // 학생이 있는 경우
+                    if (temp[nx][y] == 1) {
+                        return false;
+                    }
+                    // 장애물이 있는 경우
+                    if (temp[nx][y] == 3) {
+                        break;
+                    }
+                    nx -= 1;
+                }
             }
-            // student: 1, teacher: 2, obstacle: 3
-            if (temp[nx][ny] == 0) {
-                temp[nx][ny] = 2;
-                dfs(nx, ny);
-            } else if (temp[nx][ny] == 1) {
-                return false;
+            // 아래쪽 방향으로 감시
+            if (i == 1) {
+                nx = x + 1;
+                while (nx < n) {
+                    // 학생이 있는 경우
+                    if (temp[nx][y] == 1) {
+                        return false;
+                    }
+                    // 장애물이 있는 경우
+                    if (temp[nx][y] == 3) {
+                        break;
+                    }
+                    nx += 1;
+                }
+            }
+            // 왼쪽 방향으로 감시
+            if (i == 2) {
+                ny = y + (-1);
+                while (ny >= 0) {
+                    // 학생이 있는 경우
+                    if (temp[x][ny] == 1) {
+                        return false;
+                    }
+                    // 장애물이 있는 경우
+                    if (temp[x][ny] == 3) {
+                        break;
+                    }
+                    ny -= 1;
+                }
+            }
+            // 오른쪽 방향으로 감시
+            if (i == 3) {
+                ny = y + 1;
+                while (ny < n) {
+                    // 학생이 있는 경우
+                    if (temp[x][ny] == 1) {
+                        return false;
+                    }
+                    // 장애물이 있는 경우
+                    if (temp[x][ny] == 3) {
+                        break;
+                    }
+                    ny += 1;
+                }
             }
         }
         return true;
@@ -76,14 +125,14 @@ public class Q20 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         n = sc.nextInt();
-        arr = new int[n][n];
+        board = new int[n][n];
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 String str = sc.next();
                 if (str.equals("S")) {
-                    arr[i][j] = 1;
+                    board[i][j] = 1;
                 } else if (str.equals("T")) {
-                    arr[i][j] = 2;
+                    board[i][j] = 2;
                 }
             }
         }
